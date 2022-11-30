151064e2e195cf010868aaa8a08bc7f941bdc7c6
Adam Fisher <amgfisher90@gmail.com>
2022-11-30 07:32:57 -0500

    commit tree d68c59a652dc07ced7603cb932fe120d12225a1f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+// EVERYTHING NEEDS TO GO IN HERE, BECAUSE WE NEED TO WAIT FOR LAST EVENT LISTENER, DOM CONTENT FULLY LOADED
+
+const inputForm = document.querySelector('form');
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = document.querySelector('#searchByID');
+
+fetch(`http://localhost:3000/movies/${input.value}`)
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+
+    const title = document.querySelector('section#movieDetails h4');
+    const summary =document.querySelector('section#movieDetails p');
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+});
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
